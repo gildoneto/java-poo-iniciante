fac3177e8c3d5aa94da7128f1375c3a5330719e4
Gildo Neto <gildo.neto@telus.com>
2022-11-21 15:45:11 -0300

    aula 04

---
+++ src/aula04/Aula04.java
@@ -0,0 +1,10 @@
+package aula04;
+
+class Aula04 {
+  public static void main(String[] args) {
+    Caneta c1 = new Caneta("BIC", "Azul" ,0.7f);
+    c1.status();
+    Caneta c2 = new Caneta("ZIX", "Laranja", 1.5f);
+    c2.status();
+  }
+}
