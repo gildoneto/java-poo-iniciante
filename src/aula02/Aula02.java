7db273fe3430c7c69f8c9f47f6e9b7ceac90df98
Gildo Neto <gildo.neto@telus.com>
2022-11-21 11:22:55 -0300

    aula 02

--- src/aula02/Aula02.java
+++ src/aula02/Aula02.java
@@ -2,6 +2,13 @@ package aula02;
 
 class Aula02 {
   public static void main(String[] args) {
-
+    Caneta c1 = new Caneta();
+    c1.modelo = "BIC";
+    c1.cor = "Azul";
+    c1.ponta = 0.7f;
+    c1.carga = 90;
+    c1.modelo = "BIC";
+    c1.tampar();
+    c1.rabiscar();
   }
 }
--- src/aula02/Caneta.java
+++ src/aula02/Caneta.java
@@ -1,5 +1,33 @@
 package aula02;
 
 public class Caneta {
+  public String modelo;
+  public String cor;
+  public float ponta;
+  public int carga;
+  public boolean tampada;
+
+  public void status() {
+    System.out.println("Modelo: " + this.modelo);
+    System.out.println("A caneta " + this.cor);
+    System.out.println("Ponta: " + this.ponta);
+    System.out.println("Carga: " + this.carga);
+    System.out.println("Está tampada? " + this.tampada);
+  }
+
+  public void rabiscar() {
+    if(this.tampada == true) {
+      System.out.println("ERRO! Não posso rabiscar.");
+    } else {
+      System.out.println("Estou rabiscando");
+    }
+  }
+
+  public void tampar() {
+    this.tampada = true;
+  }
   
+  public void destampar() {
+    this.tampada = false;
+  }
 }
