fa6d3dc839dfaefa66430dafd4922b5f46c11c7c
Gildo Neto <gildo.neto@telus.com>
2022-11-21 22:08:51 -0300

    Aula 06

--- src/aula06/Aula06.java
+++ src/aula06/Aula06.java
@@ -2,6 +2,11 @@ package aula06;
 
 class Aula06 {
   public static void main(String[] args) {
-    
+    ControleRemoto c = new ControleRemoto();
+    c.ligar();
+    c.abrirMenu();
+    c.ligarMudo();
+    c.abrirMenu();
+    c.fecharMenu();
   }
 }
